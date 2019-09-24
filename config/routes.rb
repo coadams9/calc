6a1bf3aa7122031eb51f50f0f00b37e72c9c2044
Cory <coadams9@gmail.com>
2019-09-24 16:08:54 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :calcs, only: [:new]
+  
+  root :to => 'calcs#index'
 
-  post '/add', to: 'calcs#add'
-  post '/subtract', to: 'calcs#subtract'
-  post '/multiply', to: 'calcs#multiply'
-  post '/divide', to: 'calcs#divide'
+  post '/calculate', to: 'calcs#calculate'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
