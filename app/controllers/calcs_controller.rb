456071df9a816b8f5bef3a10fe2bb3cab7cc967c
Cory <coadams9@gmail.com>
2019-09-27 17:27:49 -0400

    finished

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,34 +1,124 @@
 class CalcsController < ApplicationController
-    @@result = nil
 
     def index
-
+        @result = nil
     end
-
+    
     def calculate
         user_input = params[:numbers]       
         arr = user_input.split(/(-|\*|\/|\+)/)
 
-        math = arr.map do |x|
-            if x.to_i != 0
-                x = x.to_i
-            elsif x == '/'
-                x = /\// 
-            elsif x == '*'
-                x = /\*/
-            elsif x == '-'
-                x = /-/
-            elsif x == '+'
-                x = /\+/
+        def add(a, b)
+           a + b
+        end
+
+        def multiply(a, b)
+           a * b
+        end
+
+        def subtract(a, b)
+            a - b
+        end
+
+        def divide(a, b)
+            a / b
+        end
+
+        num1 = nil
+        num2 = nil
+        
+
+        def times_divide(arr)
+            for ele, index in arr.each_with_index
+                if ele == '*'
+                    num1 = arr[index - 1].to_i
+                    num2 = arr[index + 1].to_i
+                    arr[index] = multiply(num1, num2)
+                    arr.delete_at(index - 1)
+                    arr.delete_at(index)
+                elsif ele == '/'
+                    num1 = arr[index - 1].to_i
+                    num2 = arr[index + 1].to_i
+                    arr[index] = divide(num1, num2)
+                    arr.delete_at(index - 1)
+                    arr.delete_at(index)
+                end
+            end
+        end
+        
+        def add_subtract(arr)
+            for ele, index in arr.each_with_index
+                if ele == '+'
+                    num1 = arr[index - 1].to_i
+                    num2 = arr[index + 1].to_i
+                    arr[index] = add(num1, num2)
+                    arr.delete_at(index - 1)
+                    arr.delete_at(index)
+                elsif ele == '-'
+                    num1 = arr[index - 1].to_i
+                    num2 = arr[index + 1].to_i
+                    arr[index] = subtract(num1, num2)
+                    arr.delete_at(index - 1)
+                    arr.delete_at(index) 
+                end
             end
         end
 
-       
-        byebug
+        times_divide(arr)
+        add_subtract(arr)
+
+        if arr.include?('*') || arr.include?('/')
+            times_divide(arr)
+        elsif arr.include?('+') || arr.include?('-')
+            add_subtract(arr)
+        end
+
+        @result = arr[0]
+        render :index, {result: @result} 
     end
 
 end
 
+# [15, "+", "1", "+", 3, "+", 900]
+
+
+# ["5", "*", "3", "+", "1", "+", "6", "/", "2", "+", "9", "*", "100"]
+
+# math = arr.map do |x|
+#     if x.to_i != 0
+#         x = x.to_i
+#     elsif x == '/'
+#         x = /\// 
+#     elsif x == '*'
+#         x = '*'
+#     elsif x == '-'
+#         x = /-/
+#     elsif x == '+'
+#         x = /\+/
+#     end
+# end
 
 
 
+
+
+# if !ele.match(/\+|-|\*|\//) 
+#     num1 = ele.to_i 
+#     math = eval(arr[index + 1])
+#     num2 = arr[index + 2].to_i
+
+#     if num1 && num2 && math 
+#         if math == '*'
+#             @@result = multiply(num1, num2)
+#         elsif math == '/'
+#             @@result = divide(num1, num2)
+#         elsif math == '+'
+#             @@result = add(num1, num2)
+#         elsif math == '-'
+#             @@result = subtract(num1, num2)
+#         end
+
+#     end
+
+#     byebug
+# end
